38431739e96e4daa97a54036845d7cb4db0bfeb2
Marina20V <meri20vit@gmail.com>
2022-01-06 12:46:22 +0200

    fix: fix issue with events

--- routes/events.js
+++ routes/events.js
@@ -1,23 +1,23 @@
-var express = require('express');
-var router = express.Router();
-var events = require('../bin/mockups/Data');
+// var express = require('express');
+// var router = express.Router();
+// var events = require('../bin/mockups/Data');
 
-/* GET users listing. */
+// /* GET users listing. */
 
-router.use((req, res, next) => {
-  console.log('Middleware 2');
-  next();
-});
+// router.use((req, res, next) => {
+//   console.log('Middleware 2');
+//   next();
+// });
 
-router.use((req, res, next)  => {
-  console.log('Middleware 1');
-  next();
-});
+// router.use((req, res, next)  => {
+//   console.log('Middleware 1');
+//   next();
+// });
 
-router.get('/events', function(req, res, next) {
+// router.get('/events', function(req, res, next) {
 
-  res.json(events);
+//   res.json(events);
   
-});
+// });
 
-module.exports = router;
+// module.exports = router;
