c133e4f767ec5ecf94c58555ac7cfda5ecda8442
Marina20V <meri20vit@gmail.com>
2022-01-08 21:29:30 +0200

    feat: connect images to frontend

--- app.js
+++ app.js
@@ -47,21 +47,53 @@ import db from "./config/database.js";
 import eventRoutes from "./routes/index.js";
 import categoryRoutes from "./routes/category.js";
 import cors from "cors";
- 
+import fileUpload from 'express-fileupload';
+// import { fileURLToPath } from 'url';
+// import { dirname } from 'path';
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+import path from "path"
+const __dirname = path.resolve();
+
+
+
 const app = express();
- 
+app.use(fileUpload());
+
+
 try {
-    await db.authenticate();
-    console.log('Database connected...');
+  await db.authenticate();
+   console.log('Database connected...');
 } catch (error) {
-    console.error('Connection error:', error);
+   console.error('Connection error:', error);
 }
- 
+
+
+//upload endpoint
+app.post('/upload', (req, res) => {
+    if(req.files === null) {
+        return res.status(400).json ({msg: 'No file uploaded'});
+    }
+
+    const file = req.files.file;
+    file.mv(`${__dirname}/public/uploads/${file.name}`, err=> {
+        if (err) {
+            console.error(err);
+            // return res.status(500).send(err);
+        }
+
+        res.json({fileName: file.name, filePath: `/uploads/${file.name}` });
+    });
+
+});
+
 app.use(cors());
 app.use(express.json());
 app.use('/events', eventRoutes);
 app.use('/categories', categoryRoutes);
+app.use('/uploads', express.static('uploads'));
 
 
- 
 app.listen(5000, () => console.log('Server running at port 5000'));
